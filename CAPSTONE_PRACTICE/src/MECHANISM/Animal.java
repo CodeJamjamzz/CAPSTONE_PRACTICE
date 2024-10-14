f9c8cf2b54696ef122d2ef0d9095e543254d5604
Jamiel Kyne Pinca <jamiel062705@gmail.com>
2024-10-14 22:23:09 +0800

    Practice Github Updated Version2

--- CAPSTONE_PRACTICE/src/MECHANISM/Animal.java
+++ CAPSTONE_PRACTICE/src/MECHANISM/Animal.java
@@ -2,4 +2,24 @@ package MECHANISM;
 
 public class Animal {
     // Practice to know this is the updated version
+    public String name;
+    public int age;
+
+    public Animal(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString(){
+        return name + " and i am " + age + " years old";
+    }
+
+    // this is for the new branch
+    public void talk(){
+        System.out.println("Hi i am " + name);
+    }
+
+    public int t(){
+        return 32323;
+    }
 }
